dac315496e6883d482eae3801ee9fec0de79575a
Peter Williams <peter@newton.cx>
2020-04-16 11:21:03 -0400

    recipe/build.sh: remove Linux build cruft and attempt to fix Python 3.6 build

--- recipe/build.sh
+++ recipe/build.sh
@@ -19,13 +19,13 @@ if [ $PY3K -ne 0 ] ; then
     cmake_args+=(
         -DBUILD_PYTHON3=ON
         -DBUILD_PYTHON=OFF
-        -DPYTHON3_EXECUTABLE=$PYTHON
+        -DPython3_EXECUTABLE="$PYTHON"
     )
 else
     cmake_args+=(
         -DBUILD_PYTHON=ON
         -DBUILD_PYTHON3=OFF
-        -DPYTHON2_EXECUTABLE=$PYTHON
+        -DPython2_EXECUTABLE="$PYTHON"
     )
 fi
 
@@ -39,26 +39,6 @@ if [ $(uname) = Darwin ] ; then
     )
 else
     export LDFLAGS="$LDFLAGS -Wl,-rpath-link,$PREFIX/lib"
-    # Ermahgerd, CMake and the new C++ compilers do *not* play well together.
-    # Although that may be mostly due to funky rules in this particular
-    # package. Anyway, commence hacks to get this compiling. The basic problem
-    # is that CMake wants to find paths to libraries itself. Annoyingly it is
-    # *very* hard to debug what exactly it is doing as it does so.
-
-    if [ "$cxx_compiler" = "gxx" ] ; then
-        sysroot="$BUILD_PREFIX/$HOST/sysroot"
-
-        cmake_args+=(
-            -DCMAKE_LIBRARY_PATH="$PREFIX/lib;$sysroot/lib"
-            -DPTHREADS_INCLUDE_DIR="$sysroot/usr/include"
-        )
-
-        pushd $sysroot/lib
-        for lib in dl m pthread rt ; do
-            ln -s lib${lib}.so.? lib${lib}.so
-        done
-        popd
-    fi
 fi
 
 cmake_args+=(
@@ -67,9 +47,6 @@ cmake_args+=(
 )
 
 ### cmake_args+=(--debug-trycompile --debug-output --trace-expand)
-
-# Work around bug in conda-forge package of bison (3.0.5 hfc679d8_1)
-export M4=$BUILD_PREFIX/bin/m4
 
 mkdir build
 cd build
